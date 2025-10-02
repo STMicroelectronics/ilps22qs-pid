515888b9adae9466af0fc606e6d0ac0a74348027
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2025-10-02 13:59:46 +0200

    Added checks before writes to avoid random content from being written
    The related issue is Issue #10 in lis2dux12-pid repository (see
    https://github.com/STMicroelectronics/lis2dux12-pid/issues/10).
    
    During updates, namely a read and a write on the same content,
    there were no checks to guarantee that reads have retrived the data,
    and the writes could use random content when this issue happens.
    The same fix is also applied when a pointer is changed based on the
    result of a read.
    
    Signed-off-by: Gioele Fiorenza <gioele.fiorenza2000@gmail.com>

--- ilps22qs_reg.c
+++ ilps22qs_reg.c
@@ -224,6 +224,10 @@ int32_t ilps22qs_bus_mode_get(const stmdev_ctx_t *ctx, ilps22qs_bus_mode_t *val)
   {
     ret = ilps22qs_read_reg(ctx, ILPS22QS_I3C_IF_CTRL,
                             (uint8_t *)&i3c_if_ctrl, 1);
+    if (ret != 0)
+    {
+      return ret;
+    }
 
     switch (if_ctrl.i2c_i3c_dis << 1)
     {
@@ -399,6 +403,10 @@ int32_t ilps22qs_status_get(const stmdev_ctx_t *ctx, ilps22qs_stat_t *val)
     ret = ilps22qs_read_reg(ctx, ILPS22QS_INTERRUPT_CFG,
                             (uint8_t *)&interrupt_cfg, 1);
   }
+  if (ret != 0)
+  {
+    return ret;
+  }
   val->sw_reset  = ctrl_reg2.swreset;
   val->boot      = int_source.boot_on;
   val->drdy_pres = status.p_da;
@@ -451,6 +459,10 @@ int32_t ilps22qs_pin_conf_get(const stmdev_ctx_t *ctx, ilps22qs_pin_conf_t *val)
 
   ret = ilps22qs_read_reg(ctx, ILPS22QS_IF_CTRL, (uint8_t *)&if_ctrl, 1);
 
+  if (ret != 0)
+  {
+    return ret;
+  }
   val->sda_pull_up  = if_ctrl.sda_pu_en;
   val->cs_pull_up = ~if_ctrl.cs_pu_dis;
 
@@ -485,6 +497,10 @@ int32_t ilps22qs_all_sources_get(const stmdev_ctx_t *ctx,
                             (uint8_t *)&fifo_status2, 1);
   }
 
+  if (ret != 0)
+  {
+    return ret;
+  }
   val->drdy_pres        = status.p_da;
   val->drdy_temp        = status.t_da;
   val->over_pres        = int_source.ph;
@@ -762,6 +778,11 @@ int32_t ilps22qs_ah_qvar_en_get(const stmdev_ctx_t *ctx, uint8_t *val)
   int32_t ret;
 
   ret = ilps22qs_read_reg(ctx, ILPS22QS_CTRL_REG3, (uint8_t *)&ctrl_reg3, 1);
+  if (ret != 0)
+  {
+    return ret;
+  }
+
   *val = ctrl_reg3.ah_qvar_en;
 
   return ret;
@@ -783,6 +804,10 @@ int32_t ilps22qs_data_get(const stmdev_ctx_t *ctx, ilps22qs_md_t *md,
   int32_t ret;
 
   ret = ilps22qs_read_reg(ctx, ILPS22QS_PRESS_OUT_XL, buff, 5);
+  if (ret != 0)
+  {
+    return ret;
+  }
 
   /* pressure conversion */
   data->pressure.raw = (int32_t)buff[2];
@@ -856,6 +881,11 @@ int32_t ilps22qs_pressure_raw_get(const stmdev_ctx_t *ctx, uint32_t *buff)
   uint8_t reg[3];
 
   ret =  ilps22qs_read_reg(ctx, ILPS22QS_PRESS_OUT_XL, reg, 3);
+  if (ret != 0)
+  {
+    return ret;
+  }
+
   *buff = reg[2];
   *buff = (*buff * 256U) + reg[1];
   *buff = (*buff * 256U) + reg[0];
@@ -878,6 +908,11 @@ int32_t ilps22qs_temperature_raw_get(const stmdev_ctx_t *ctx, int16_t *buff)
   uint8_t reg[2];
 
   ret =  ilps22qs_read_reg(ctx, ILPS22QS_TEMP_OUT_L, reg, 2);
+  if (ret != 0)
+  {
+    return ret;
+  }
+
   *buff = (int16_t)reg[1];
   *buff = (*buff * 256) + (int16_t)reg[0];
 
@@ -900,6 +935,10 @@ int32_t ilps22qs_ah_qvar_data_get(const stmdev_ctx_t *ctx,
   int32_t ret;
 
   ret = ilps22qs_read_reg(ctx, ILPS22QS_PRESS_OUT_XL, buff, 3);
+  if (ret != 0)
+  {
+    return ret;
+  }
 
   /* QVAR conversion */
   data->raw = (int32_t)buff[2];
@@ -964,6 +1003,10 @@ int32_t ilps22qs_fifo_mode_get(const stmdev_ctx_t *ctx, ilps22qs_operation_t *va
   int32_t ret;
 
   ret = ilps22qs_read_reg(ctx, ILPS22QS_FIFO_CTRL, (uint8_t *)&fifo_ctrl, 1);
+  if (ret != 0)
+  {
+    return ret;
+  }
 
   switch ((fifo_ctrl.trig_modes << 2) | fifo_ctrl.f_mode)
   {
@@ -1100,6 +1143,10 @@ int32_t ilps22qs_fifo_level_get(const stmdev_ctx_t *ctx, uint8_t *val)
 
   ret = ilps22qs_read_reg(ctx, ILPS22QS_FIFO_STATUS1,
                           (uint8_t *)&fifo_status1, 1);
+  if (ret != 0)
+  {
+    return ret;
+  }
 
   *val = fifo_status1.fss;
 
@@ -1125,7 +1172,11 @@ int32_t ilps22qs_fifo_data_get(const stmdev_ctx_t *ctx, uint8_t samp,
 
   for (i = 0U; i < samp; i++)
   {
-    ret = ilps22qs_read_reg(ctx, ILPS22QS_FIFO_DATA_OUT_PRESS_XL, fifo_data, 3);
+    ret += ilps22qs_read_reg(ctx, ILPS22QS_FIFO_DATA_OUT_PRESS_XL, fifo_data, 3);
+    if (ret != 0)
+    {
+      return ret;
+    }
     data[i].raw = (int32_t)fifo_data[2];
     data[i].raw = (data[i].raw * 256) + (int32_t)fifo_data[1];
     data[i].raw = (data[i].raw * 256) + (int32_t)fifo_data[0];
@@ -1231,6 +1282,10 @@ int32_t ilps22qs_interrupt_mode_get(const stmdev_ctx_t *ctx,
 
   ret += ilps22qs_read_reg(ctx, ILPS22QS_INTERRUPT_CFG,
                            (uint8_t *)&interrupt_cfg, 1);
+  if (ret != 0)
+  {
+    return ret;
+  }
 
   val->int_latched = interrupt_cfg.lir;
 
@@ -1324,6 +1379,10 @@ int32_t ilps22qs_int_on_threshold_mode_get(const stmdev_ctx_t *ctx,
   int32_t ret;
 
   ret = ilps22qs_read_reg(ctx, ILPS22QS_INTERRUPT_CFG, reg, 3);
+  if (ret != 0)
+  {
+    return ret;
+  }
 
   bytecpy((uint8_t *)&interrupt_cfg, &reg[0]);
   bytecpy((uint8_t *)&ths_p_l, &reg[1]);
@@ -1395,6 +1454,10 @@ int32_t ilps22qs_reference_mode_get(const stmdev_ctx_t *ctx, ilps22qs_ref_md_t *
 
   ret = ilps22qs_read_reg(ctx, ILPS22QS_INTERRUPT_CFG,
                           (uint8_t *)&interrupt_cfg, 1);
+  if (ret != 0)
+  {
+    return ret;
+  }
 
   switch ((interrupt_cfg.reset_az << 1) |
           interrupt_cfg.autorefp)
@@ -1450,6 +1513,10 @@ int32_t ilps22qs_opc_get(const stmdev_ctx_t *ctx, int16_t *val)
   int32_t ret;
 
   ret = ilps22qs_read_reg(ctx, ILPS22QS_RPDS_L, reg, 2);
+  if (ret != 0)
+  {
+    return ret;
+  }
 
   *val = (int16_t)reg[1];
   *val = *val * 256 + (int16_t)reg[0];
